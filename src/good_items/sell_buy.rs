f761c1a6003b0d20c435666563a240374256d715
LokiVKlokeNaAndoke <ma.ba1@rambler.ru>
2021-12-07 12:40:45 +0200

    careful

--- src/good_items/sell_buy.rs
+++ src/good_items/sell_buy.rs
@@ -25,7 +25,7 @@ pub fn get_good_items_sell_buy(
             let src_avgs = averages(config, &x.source.history);
             let dst_avgs = averages(config, &x.destination.history);
 
-            let (recommend_buy_vol, dest_sell_price, max_buy_price) = {
+            let (recommend_buy_vol, dest_sell_price, max_buy_price, avg_buy_price) = {
                 let mut source_sell_orders = x
                     .source
                     .orders
@@ -39,6 +39,7 @@ pub fn get_good_items_sell_buy(
                 let mut recommend_bought_volume = 0;
                 let mut sum_sell_price = 0.;
                 let mut max_buy_price = 0.;
+                let mut sum_buy_price = 0.;
                 'outer: for buy_order in x
                     .destination
                     .orders
@@ -64,6 +65,7 @@ pub fn get_good_items_sell_buy(
                         if expenses >= sell_price {
                             break;
                         }
+                        sum_buy_price += curr_src_sell_order.price * bought_volume as f64;
                         curr_src_sell_order.volume_remain -= bought_volume;
                         max_buy_price = curr_src_sell_order.price.max(max_buy_price);
                         sum_sell_price += buy_order.price * bought_volume as f64;
@@ -83,13 +85,13 @@ pub fn get_good_items_sell_buy(
                     recommend_bought_volume,
                     sum_sell_price / recommend_bought_volume as f64,
                     max_buy_price,
+                    sum_buy_price / recommend_bought_volume as f64,
                 )
             };
 
             // multibuy can only buy at a fixed price, so all buys from multiple sell orders
             // with different prices have you paid the same price for all of them
             let expenses = max_buy_price;
-
             let buy_with_broker_fee = expenses * (1. + config.broker_fee_source);
             let fin_expenses = buy_with_broker_fee
                 + x.desc.volume.unwrap() as f64 * config.freight_cost_iskm3
@@ -100,10 +102,24 @@ pub fn get_good_items_sell_buy(
 
             let rough_profit = (fin_sell_price - fin_expenses) * recommend_buy_vol as f64;
 
+            // also calculate avg buy price
+            let best_expenses = avg_buy_price;
+            let buy_with_broker_fee = best_expenses * (1. + config.broker_fee_source);
+            let fin_expenses = buy_with_broker_fee
+                + x.desc.volume.unwrap() as f64 * config.freight_cost_iskm3
+                + buy_with_broker_fee * config.freight_cost_collateral_percent;
+            let fin_sell_price = dest_sell_price * (1. - config.sales_tax);
+
+            let best_margin = (fin_sell_price - fin_expenses) / fin_expenses;
+
+            let best_rough_profit = (fin_sell_price - fin_expenses) * recommend_buy_vol as f64;
+
             Some(PairCalculatedDataSellBuy {
                 market: x,
                 margin,
+                best_margin,
                 rough_profit,
+                best_rough_profit,
                 market_dest_volume: dst_mkt_volume,
                 recommend_buy: recommend_buy_vol,
                 expenses: fin_expenses,
@@ -116,11 +132,11 @@ pub fn get_good_items_sell_buy(
             })
         })
         .flatten()
-        .filter(|x| x.margin > config.margin_cutoff)
+        .filter(|x| x.best_margin > config.margin_cutoff)
         .filter(|x| {
             x.src_avgs.volume > config.min_src_volume && x.dst_avgs.volume > config.min_dst_volume
         })
-        .sorted_unstable_by_key(|x| NotNan::new(-x.rough_profit).unwrap())
+        .sorted_unstable_by_key(|x| NotNan::new(-x.best_rough_profit).unwrap())
         .take(config.items_take)
         .collect::<Vec<_>>()
 }
@@ -142,6 +158,7 @@ pub fn make_table_sell_buy<'a, 'b>(
         TableCell::new("mkt src"),
         TableCell::new("mkt dst"),
         TableCell::new("rough prft"),
+        TableCell::new("crfl buy"),
         TableCell::new("rcmnd vlm"),
     ]))
     .chain(good_items.iter().map(|it| {
@@ -160,6 +177,14 @@ pub fn make_table_sell_buy<'a, 'b>(
             TableCell::new(format!("{:.2}", it.market_src_volume)),
             TableCell::new(format!("{:.2}", it.market_dest_volume)),
             TableCell::new(format!("{:.2}", it.rough_profit)),
+            TableCell::new(format!(
+                "{}",
+                if (it.best_rough_profit - it.rough_profit) / it.rough_profit > 0.1 {
+                    "yes"
+                } else {
+                    ""
+                }
+            )),
             TableCell::new(format!("{}", it.recommend_buy)),
         ])
     }))
@@ -180,4 +205,6 @@ pub struct PairCalculatedDataSellBuy {
     pub src_avgs: ItemTypeAveraged,
     pub dst_avgs: ItemTypeAveraged,
     pub market_src_volume: i32,
+    best_rough_profit: f64,
+    best_margin: f64,
 }
