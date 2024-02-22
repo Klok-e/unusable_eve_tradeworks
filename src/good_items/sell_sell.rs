5f898d1e20350d8a823657b9fe1b1c4accad6283
LokiVKlokeNaAndoke <dmytro.sokolenko@tutanota.com>
2024-02-22 19:31:55 +0200

    tweak sell price calc

--- src/good_items/sell_sell.rs
+++ src/good_items/sell_sell.rs
@@ -279,15 +279,23 @@ pub fn calculate_sell_price(
     }
 
     match (dst_lowest_sell_order, dst_avgs) {
-        (Some(dst_lowest_sell_order), Some(dst_avgs))
+        (Some(dst_lowest_sell_order), Some(dst_avgs)) => {
             if dst_lowest_sell_order > dst_avgs.high_average
-                && ((dst_lowest_sell_order - dst_avgs.high_average) / dst_avgs.high_average)
-                    > config.ignore_difference_between_history_and_order_pct =>
-        {
-            dst_avgs.high_average
+            {
+                return dst_avgs.high_average;
+            }
+
         }
-        (Some(dst_lowest_sell_order), _) => outbid_price(dst_lowest_sell_order, false),
-        (_, Some(dst_avgs)) => dst_avgs.high_average.max(sell_with_markup),
-        (_, _) => sell_with_markup,
+        _ => {}
+    };
+
+    if let Some(dst_lowest_sell_order) = dst_lowest_sell_order {
+        return outbid_price(dst_lowest_sell_order, false);
     }
+
+    if let Some(dst_avgs) = dst_avgs {
+        return dst_avgs.high_average.max(sell_with_markup);
+    }
+
+    sell_with_markup
 }
